35b6d48690a110ff4b1b87605151ed3be814b51c
Eloi Isern <nuntxaku@gmail.com>
2021-10-04 12:27:43 +0200

    primera pujada

---
+++ 1_queries_tienda.sql
@@ -0,0 +1,42 @@
+SELECT nombre FROM producto;
+SELECT nombre, precio FROM producto;
+SELECT * FROM tienda.producto;
+SELECT nombre, precio, precio * 1.17  FROM producto;
+SELECT nombre, precio AS 'precio €', precio * 1.17 AS 'precio $' FROM producto;
+SELECT nombre, UPPER (nombre), precio FROM producto;
+SELECT nombre, LOWER (nombre), precio FROM producto;
+SELECT nombre, UPPER(LEFT(nombre,2)) FROM fabricante;
+SELECT nombre, ROUND (precio) FROM producto;
+SELECT nombre, TRUNCATE (precio, 0) FROM producto;
+SELECT codigo_fabricante FROM producto;
+SELECT DISTINCT codigo_fabricante FROM producto;
+SELECT nombre FROM fabricante;
+SELECT nombre FROM fabricante ORDER BY nombre ASC;
+SELECT nombre FROM fabricante ORDER BY nombre DESC;
+SELECT * FROM fabricante LIMIT 5;
+SELECT * FROM fabricante LIMIT 3, 2;
+SELECT *  FROM producto ORDER BY precio ASC LIMIT 1;
+SELECT *  FROM producto ORDER BY precio DESC LIMIT 1;
+FROM producto WHERE codigo_fabricante = 2;
+SELECT p.nombre, p.precio, f.nombre FROM producto p JOIN fabricante f ON p.codigo = f.codigo;
+SELECT p.nombre, p.precio,  f.nombre FROM producto p LEFT JOIN fabricante f ON p.codigo = f.codigo ORDER BY f.nombre;
+SELECT p.codigo, p.nombre, f.codigo,  f.nombre  FROM producto p LEFT JOIN fabricante f ON p.codigo = f.codigo;
+SELECT p.nombre, p.precio, f.nombre FROM producto p JOIN fabricante f ON p.codigo = f.codigo ORDER BY precio ASC LIMIT 1;
+SELECT p.nombre, p.precio, f.nombre FROM producto p JOIN fabricante f ON p.codigo = f.codigo ORDER BY precio DESC LIMIT 1;
+SELECT * FROM producto p JOIN fabricante f ON p.codigo_fabricante = f.codigo AND f.nombre = 'Lenovo';
+SELECT * FROM producto p JOIN fabricante f ON p.codigo_fabricante = f.codigo AND f.nombre = 'Crucial' AND precio >=200;
+SELECT * FROM producto p JOIN fabricante f ON p.codigo_fabricante = f.codigo WHERE (f.nombre = 'Asus') OR (f.nombre = 'Hewlett-Packard') OR (f.nombre = 'Seagate');
+SELECT *  FROM producto p JOIN fabricante f ON p.codigo_fabricante = f.codigo WHERE f.nombre IN ('Asus', 'Hewlett-Packard','Seagate');
+SELECT p.nombre, p.precio, f.nombre FROM producto p JOIN fabricante f ON p.codigo_fabricante = f.codigo AND  f.nombre LIKE '%e';
+SELECT p.nombre, p.precio, f.nombre FROM producto p JOIN fabricante f ON p.codigo_fabricante = f.codigo AND  f.nombre LIKE '%w%';
+SELECT p.nombre, p.precio, f.nombre FROM producto p JOIN fabricante f ON p.codigo_fabricante = f.codigo AND  p.precio >= 180 ORDER BY p.precio DESC, p.nombre ASC;
+SELECT DISTINCT f.codigo, f.nombre FROM producto p JOIN fabricante f ON p.codigo_fabricante = f.codigo;
+SELECT f.nombre AS Fabricante, p.nombre AS Producto FROM fabricante f LEFT JOIN producto p ON p.codigo_fabricante = f.codigo;
+SELECT f.nombreFROM   fabricante fWHERE  NOT EXISTS (SELECT * FROM producto p WHERE p.codigo_fabricante = f.codigo);
+SELECT p.nombre AS 'productes' FROM   producto p WHERE   p.codigo_fabricante = (SELECT codigo FROM  fabricante f WHERE  f.nombre = "Lenovo");
+SELECT * FROM producto pe WHERE pe.precio = (SELECT p.precio FROM   producto p WHERE p.codigo_fabricante = (SELECT codigo FROM   fabricante f WHERE  f.nombre = "LENOVO") ORDER BY precio DESC LIMIT 1);
+
+SELECT p.nombre, p.precio FROM producto p WHERE p.codigo_fabricante = (SELECT codigo FROM fabricante f WHERE  f.nombre = "Lenovo") ORDER BY precio DESC LIMIT 1; -- Ho hauria fet amb SELECT MAX(p.precio) però no em funciona. Expulsa error 1140 i em diu que hi troba a faltar GROUP BY
+SELECT p.nombre, MIN (p.precio) FROM producto p WHERE p.codigo_fabricante = (SELECT codigo FROM   fabricante f WHERE  f.nombre = "Hewlett-Packard");
+SELECT * FROM producto pe WHERE pe.precio >= (SELECT p.precio FROM   producto p WHERE p.codigo_fabricante = (SELECT codigo FROM   fabricante f WHERE  f.nombre = "LENOVO") ORDER BY precio DESC LIMIT 1);
+
